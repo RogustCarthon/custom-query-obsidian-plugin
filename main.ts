18434fc5a4e9d8236f4f8f8c6321d78dba798618
RogustCarthon <jay.k.n.bhatt@gmail.com>
2024-10-06 14:37:07 +0530

    Created custom query plugin.

--- main.ts
+++ main.ts
@@ -1,134 +1,105 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin } from "obsidian";
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async onload() {
+    this.registerMarkdownCodeBlockProcessor(
+      "md-query",
+      async (source, el, ctx) => {
+        // Parts is an array of key value pairs of the query.
+        const parts = source.split("\n");
+        const partsMap = new Map<string, string>();
+        parts.forEach((part) => {
+          const [key, value] = part.split(":").map((str) => str.trim());
+          if (key && value) {
+            partsMap.set(key, value);
+          }
+        });
+        const headingText = partsMap.get("heading");
+        const headingLevel = partsMap.get("level");
+        const prefix = partsMap.get("path");
 
-	async onload() {
-		await this.loadSettings();
+        if (!headingText) {
+          throw new Error("Missing 'heading' value in the query.");
+        }
+        if (!headingLevel) {
+          throw new Error("Missing 'level' value in the query.");
+        }
+        if (!prefix) {
+          throw new Error("Missing 'prefix' value in the query.");
+        }
+        let endResult: Array<{ lines: string[]; file: string }> = [];
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const files = this.app.vault.getMarkdownFiles();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        for (const file of files) {
+          if (!file.path.startsWith(prefix)) continue;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+          const content = await this.app.vault.read(file);
+          const lines = content.split("\n");
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+          let result: string[] = [];
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+          for (let i = 0; i < lines.length; ) {
+            const line = lines[i];
+            const headingMatch =
+              line.startsWith("#".repeat(parseInt(headingLevel))) &&
+              line.includes(headingText);
+            if (!headingMatch) {
+              i++;
+              continue;
+            }
+            let nextHeadingIndex = lines.slice(i + 1).findIndex((l) => {
+              let currentLevel = 0;
+              for (const char of l) {
+                if (char === "#") {
+                  currentLevel++;
+                } else {
+                  break;
+                }
+              }
+              return currentLevel > 0 && currentLevel <= parseInt(headingLevel);
+            });
+            if (nextHeadingIndex === -1) {
+              nextHeadingIndex = lines.length;
+            } else {
+              nextHeadingIndex += i + 1;
+            }
+            result = result.concat(lines.slice(i, nextHeadingIndex));
+            i = nextHeadingIndex;
+          }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+          if (result.length > 0) {
+            endResult.push({ lines: result, file: file.path });
+          }
+        }
+        for (const res of endResult) {
+          const linkElement = document.createElement("a");
+          linkElement.href = `obsidian://open?vault=${encodeURIComponent(this.app.vault.getName())}&file=${encodeURIComponent(res.file)}`;
+          linkElement.textContent = `${res.file}`;
+          el.appendChild(linkElement);
+          for (const line of res.lines) {
+            let tag = `p`;
+            if (line.startsWith("#")) {
+              const match = line.match(/^(#+)/);
+              if (match) {
+                const level = match[1].length;
+                tag = `h${level}`;
+              }
+            }
+            const element = document.createElement(tag);
+            element.textContent = line.startsWith("#")
+              ? line.replace(/^(#+)\s*/, "")
+              : line;
+            el.appendChild(element);
+          }
+        }
+      },
+    );
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  onunload() {}
 
-	onunload() {
+  async loadSettings() {}
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  async saveSettings() {}
 }
