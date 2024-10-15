fd9216d5e194a5bf24a6cacd71eede016b649982
RogustCarthon <jay.k.n.bhatt@gmail.com>
2024-10-15 10:29:42 +0530

    Updated query to include tasks.

--- main.ts
+++ main.ts
@@ -1,76 +1,163 @@
-import { Plugin, MarkdownRenderer } from "obsidian";
+import {
+  Plugin,
+  MarkdownRenderer,
+  MarkdownPostProcessorContext,
+  TFile,
+} from "obsidian";
+
+function getQueryParts(query: string) {
+  // Parts is an array of key value pairs of the query.
+  const parts = query.split("\n");
+  const partsMap = new Map<string, string>();
+  parts.forEach((part) => {
+    const [key, value] = part.split(":").map((str) => str.trim());
+    if (key && value) {
+      partsMap.set(key, value);
+    }
+  });
+  const elementType = partsMap.get("type"); // heading or task.
+  const text = partsMap.get("text");
+  const pathPrefix = partsMap.get("path");
+  const headingLevel = partsMap.get("level"); // optional with heading.
+
+  if (!elementType) {
+    throw new Error("Missing 'type' value in the query.");
+  }
+  if (elementType != "heading" && elementType != "task") {
+    throw new Error("Invalid 'type' value in the query.");
+  }
+  if (elementType == "heading" && !headingLevel) {
+    throw new Error("Missing 'level' value in the query");
+  }
+  if (!text) {
+    throw new Error("Missing 'text' value in the query.");
+  }
+  if (!pathPrefix) {
+    throw new Error("Missing 'path' value in the query.");
+  }
+  return {
+    elementType,
+    text,
+    pathPrefix,
+    headingLevel,
+  };
+}
+
+function getSectionUnderHeading(
+  lines: string[],
+  file: TFile,
+  headingLevel: string,
+  headingText: string,
+) {
+  const result: string[] = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const headingMatch =
+      line == "#".repeat(parseInt(headingLevel)) + " " + headingText;
+    if (!headingMatch) {
+      continue;
+    }
+    let sectionEnd = lines.length;
+    for (let j = i + 1; j < lines.length; j++) {
+      const nextLine = lines[j];
+      if (
+        nextLine.startsWith("#") &&
+        nextLine.split(" ")[0].length <= parseInt(headingLevel)
+      ) {
+        sectionEnd = j;
+        break;
+      }
+    }
+    result.push(`[[${file.path}#${headingText}|${file.name}]]`);
+    result.push(...lines.slice(i + 1, sectionEnd));
+    break;
+  }
+  return result;
+}
+
+function getTasks(lines: string[], file: TFile) {
+  const result: string[] = [];
+
+  const getBlock = (
+    start: number,
+  ): [string[] | undefined, number | undefined] => {
+    if (lines[start].startsWith("\t")) {
+      return [undefined, undefined];
+    }
+    let hasTask = lines[start].startsWith("- [ ]");
+    let end = start + 1;
+    for (; end < lines.length; end++) {
+      if (lines[end].startsWith("\t")) {
+        hasTask ||= lines[end].trimStart().startsWith("- [ ]");
+      } else {
+        break;
+      }
+    }
+    if (hasTask) {
+      return [lines.slice(start, end), end];
+    }
+    return [undefined, undefined];
+  };
+
+  for (let i = 0; i < lines.length; ) {
+    const [res, end] = getBlock(i);
+    if (res) {
+      result.push(...res);
+    }
+    if (end) {
+      i = end;
+    } else {
+      i++;
+    }
+  }
+  return Array.combine([[`[[${file.path}|${file.name}]]`], result]);
+}
 
 export default class MyPlugin extends Plugin {
   async onload() {
     this.registerMarkdownCodeBlockProcessor(
       "md-query",
-      async (source, el, ctx) => {
-        // Parts is an array of key value pairs of the query.
-        const parts = source.split("\n");
-        const partsMap = new Map<string, string>();
-        parts.forEach((part) => {
-          const [key, value] = part.split(":").map((str) => str.trim());
-          if (key && value) {
-            partsMap.set(key, value);
-          }
-        });
-        const headingText = partsMap.get("heading");
-        const headingLevel = partsMap.get("level");
-        const prefix = partsMap.get("path");
+      async (
+        source: string,
+        el: HTMLElement,
+        ctx: MarkdownPostProcessorContext,
+      ) => {
+        const { elementType, text, pathPrefix, headingLevel } =
+          getQueryParts(source);
 
-        if (!headingText) {
-          throw new Error("Missing 'heading' value in the query.");
-        }
-        if (!headingLevel) {
-          throw new Error("Missing 'level' value in the query.");
-        }
-        if (!prefix) {
-          throw new Error("Missing 'prefix' value in the query.");
-        }
         const endResult: Array<{ lines: string[]; file: string }> = [];
 
-        const files = this.app.vault.getMarkdownFiles();
+        const files = this.app.vault
+          .getMarkdownFiles()
+          .filter((file) => file.path.startsWith(pathPrefix));
 
         for (const file of files) {
-          if (!file.path.startsWith(prefix)) continue;
-
           const content = await this.app.vault.read(file);
           const lines = content.split("\n");
 
-          const result: string[] = [];
+          let result: string[] = [];
 
-          for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-            const headingMatch =
-              line == "#".repeat(parseInt(headingLevel)) + " " + headingText;
-            if (!headingMatch) {
-              continue;
-            } else {
-              let sectionEnd = lines.length;
-              for (let j = i + 1; j < lines.length; j++) {
-                const nextLine = lines[j];
-                if (
-                  nextLine.startsWith("#") &&
-                  nextLine.split(" ")[0].length <= parseInt(headingLevel)
-                ) {
-                  sectionEnd = j;
-                  break;
-                }
-              }
-              result.push(`[[${file.path}#${headingText}|${file.name}]]`);
-              result.push(...lines.slice(i + 1, sectionEnd));
+          switch (elementType) {
+            case "heading":
+              result = getSectionUnderHeading(
+                lines,
+                file,
+                headingLevel || "",
+                text,
+              );
               break;
-            }
+            case "task":
+              result = getTasks(lines, file);
+              break;
           }
 
           if (result.length > 0) {
             endResult.push({ lines: result, file: file.path });
           }
         }
-        console.log(endResult);
         for (const res of endResult) {
           const joined = res.lines.join("\n");
-          console.log(joined);
           MarkdownRenderer.render(this.app, joined, el, res.file, this);
         }
       },
